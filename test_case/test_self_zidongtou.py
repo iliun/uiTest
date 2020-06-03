395a2862314008a72ad8c74cbeb1517dedac3fa4
iliun <iliun@163.com>
2020-06-03 14:16:11 +0800

    sys

--- test_case/test_self_zidongtou.py
+++ test_case/test_self_zidongtou.py
@@ -1,4 +1,6 @@
 #coding=utf-8
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 from common.com_func import Common
 from common.desired_caps import appium_desired
 from selenium.webdriver.support.ui import WebDriverWait
